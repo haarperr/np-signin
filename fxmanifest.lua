d4d037a741e1b5e68697ac68e21a3b2e2d24367b
Skull <86374920+vSkull@users.noreply.github.com>
2021-09-08 09:17:56 +0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+client_script "@np-errorlog/client/cl_errorlog.lua"
+client_script "@np-lib/client/cl_ui.lua"
+client_script "config.lua"
+
+client_scripts {
+  "client/cl_*.lua"
+}
+server_scripts {
+  "server/sv_*.lua"
+}
